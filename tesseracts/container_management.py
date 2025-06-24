6e1ebffaf1e50272536e291387aa85385c219b85
Archis Joglekar <joglekara@users.noreply.github.com>
2025-06-24 09:54:18 -0700

    updating resource requirements (#19)

--- tesseracts/container_management.py
+++ tesseracts/container_management.py
@@ -107,8 +107,8 @@ def create_or_update_tesseract_service(tesseract_name):
 def register_task_definition(ecs_client, new_container_def, family_name):
     new_task_definition = ecs_client.register_task_definition(
         family=family_name,
-        cpu="4000",
-        memory="15000",
+        cpu="2040",
+        memory="7660",
         containerDefinitions=[new_container_def],
         networkMode="awsvpc",
         requiresCompatibilities=["EC2"],
